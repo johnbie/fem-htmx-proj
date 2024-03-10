e5e6b12d8e059c25b3ba3e766aa7cd4c86aa6f3b
John Lee <johnqwerty.lee1@gmail.com>
2024-03-09 17:00:00 -0800

    Updated backend to support forms

--- cmd/main.go
+++ cmd/main.go
@@ -26,22 +26,53 @@ type Count struct {
     Count int
 }
 
+type Contact struct {
+    Name string
+    Email string
+}
+
+func newContact(name, email string) Contact {
+    return Contact {
+        Name: name,
+        Email: email,
+    }
+}
+
+type Contacts = []Contact
+
+type Data struct {
+    Contacts Contacts
+}
+
+func newData() Data {
+    return Data {
+        Contacts: []Contact {
+            newContact("John", "jd@gmail.com"),
+            newContact("Clara", "cd@gmail.com"),
+        },
+    }
+}
+
 func main() {
 
     e := echo.New()
 
-    count := Count{Count: 0}
+    data := newData()
 
     e.Renderer = newTemplate()
     e.Use(middleware.Logger())
 
     e.GET("/", func(c echo.Context) error {
-        return c.Render(200, "index", count)
+        return c.Render(200, "index", data)
     });
 
-    e.POST("/count", func(c echo.Context) error {
-        count.Count++
-        return c.Render(200, "count", count)
+    e.POST("/contacts", func(c echo.Context) error {
+        name := c.FormValue("name")
+	email := c.FormValue("email")
+
+        data.Contacts = append(data.Contacts, newContact(name, email))
+
+	return c.Render(200, "index", data)
     });
 
     e.Logger.Fatal(e.Start(":42069"))
